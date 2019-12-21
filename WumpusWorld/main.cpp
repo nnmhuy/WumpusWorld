89fd03df17720e5b08218c0efb3101c4726407c0
Minh Huy <nnmhuy@apcs.vn>
2019-12-21 23:47:59 +0700

    choose map and method

--- WumpusWorld/main.cpp
+++ WumpusWorld/main.cpp
@@ -13,27 +13,36 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
+    int method = 1;
+    string mapName;
+    
+    cout << "Please input map name: ";
+    cin >> mapName;
+    cout << "Please choose running algorithm (1.Logic, 2: Q Learning): ";
+    cin >> method;
+    
     Map map1;
-    map1.readMap("map5.txt");
-    Logger::mFilename = "logical-result5.txt";
     map1.readMap("map.txt");
     Logger::inputFilename = "map.txt";
     Logger::mFilename = "result.txt";
-//    LogicalAgent la(map1.startPoint, map1.mapSize, &map1);
-//    la.startAgent();
-////    char c;
-//    while (la.isEnd() == false) {
-//        la.draw();
-//        cin.get();
-//        la.makeMove();
-//    }
-//    la.draw();
-
-    QLearningAgent qa(map1.startPoint, map1.mapSize, &map1);
-    qa.train();
-    LogicalAgent la(map1.startPoint, map1.mapSize, &map1);
-    while (qa.isEnd() == false) {
-        qa.makeMove();
+    
+    if (method != 2) {
+        LogicalAgent la(map1.startPoint, map1.mapSize, &map1);
+        la.startAgent();
+    //    char c;
+        while (la.isEnd() == false) {
+            la.draw();
+            cin.get();
+            la.makeMove();
+        }
+        la.draw();
+    } else {
+        QLearningAgent qa(map1.startPoint, map1.mapSize, &map1);
+        qa.train();
+        LogicalAgent la(map1.startPoint, map1.mapSize, &map1);
+        while (qa.isEnd() == false) {
+            qa.makeMove();
+        }
     }
     return 0;
 }
